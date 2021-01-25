70b03d32a0ce0e58d99a8100adf23ab14c24aa25
sueda akın <sueda_akin@hotmail.com>
2021-01-25 20:58:25 +0300

    Proje dosyası ekle.

---
+++ DictionaryIntro/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryIntro
+{
+    class MyDictionary<T,S>
+    {
+        T[] items;
+        S[] items2;
+        public MyDictionary()
+        {
+            items = new T[0];
+            items2 = new S[0];
+        }
+        public void Add(T item,S item2)
+        {
+            T[] tempArray = items;
+            S[] tempArray2 = items2;
+            items = new T[items.Length + 1];
+            items2 = new S[items2.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+                items2[i] = tempArray2[i];
+            }
+            items[Count - 1] = item;
+            items2[Count - 1] = item2;
+        }
+        public int Count
+        {
+            get { return items.Length; }
+        }
+        public T[] Items
+        {
+            get { return items; }
+        }
+        public S[] Items2
+        {
+            get { return items2; }
+        }
+    }
+}
---
+++ DictionaryIntro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DictionaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string, int> adYas = new MyDictionary<string, int>();
+            adYas.Add("Sueda", 20);
+            Console.WriteLine(adYas.Count);
+            adYas.Add("Elif", 30);
+            Console.WriteLine(adYas.Count);
+            adYas.Add("Zehra", 50);
+
+            for (int i = 0; i < adYas.Count; i++)
+            {
+                for (int j = 0; j < adYas.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.WriteLine("["+adYas.Items[i]+","+adYas.Items2[j]+"]");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
